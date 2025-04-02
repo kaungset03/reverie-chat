4c48b0856d636777b49294ac879e4ea983dea4d4
Kaung Set <kset2299@gmail.com>
2025-04-02 20:25:27 +0630

    Refactor application structure by removing App component, adding utility functions, and implementing new UI components with Tailwind CSS

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-const App = () => {
-  return (
-    <main>
-      <h2>Hello World</h2>
-    </main>
-  );
-};
-export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-// @ts-expect-error process is a nodejs global
+import path from "path";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
@@ -12,6 +12,11 @@ export default defineConfig(async () => ({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
